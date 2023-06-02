3525714c68a0b0be33d471f50a43c573aa2b184a
Edwin <loaizae9@gmail.com>
2023-06-02 13:27:57 -0500

    se agrego poco

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <h1 className='font-bold text-white mt-10 text-center text-5xl'>Administrador <span className='text-yellow-600 ' >Pacientes</span></h1>
+      <h1 className='font-bold text-white mt-10 text-center text-5xl'>Administrador <span className='text-yellow-600 ' >Pacientes con GitHub</span></h1>
     </>
   )
 }
